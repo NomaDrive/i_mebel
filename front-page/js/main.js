cd4f7440295adf93baa8b425dcef9c2ab089c13d
mikkri58 <m1kkr@yandex.ua>
2020-10-28 17:33:10 +0300

    front-order

--- front-page/js/main.js
+++ front-page/js/main.js
@@ -1,1 +1,14 @@
-privet
+jQuery(document).ready(function () {
+    jQuery(".slider-preview").slick({
+        arrows: true,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+    jQuery(".slider-product-present").slick({
+        arrows: true,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+})
