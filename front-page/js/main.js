28582ea93112507b830aff773a8cf41d225adc57
Artem111299 <gurin.artem111299@gmail.com>
2020-11-20 00:59:16 +0300

    remake under-category

--- front-page/js/main.js
+++ front-page/js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function () {
+jQuery(document).ready(function ($) {
     jQuery(".slider-preview").slick({
         arrows: true,
         slidesToShow: 1,
@@ -28,5 +28,27 @@ jQuery(document).ready(function () {
         slidesToShow: 1,
         autoplay: true,
         autoplaySpeed: 1500
+    });
+
+    $(".tags__button-open").click(function(){
+        $(".tags__elements").toggleClass("open")
+        $(".tags__elements").append(this)
+
+        if($(".tags__elements").hasClass("open")){
+            $(".tags__button-open").text("Скрыть тэги")
+        }else{
+            $(".tags__button-open").text("Показать еще тэги")
+        }
+    });
+
+    $(".filtrs__button-open").click(function(){
+        $(".filtrs__elements").toggleClass("open")
+        $(".filtrs__elements").append(this)
+
+        if($(".filtrs__elements").hasClass("open")){
+            $(".filtrs__button-open").text("Скрыть тэги")
+        }else{
+            $(".filtrs__button-open").text("Показать еще тэги")
+        }
     });
 })
