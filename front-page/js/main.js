67a9c00053f8cb454eb187727da2386d062f0218
mikkri58 <m1kkr@yandex.ru>
2020-11-19 20:26:57 +0300

    swiper-front

--- front-page/js/main.js
+++ front-page/js/main.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function () {
-    jQuery(".slider-preview").slick({
-        arrows: true,
-        slidesToShow: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+    // jQuery(".slider-preview").slick({
+    //     arrows: true,
+    //     slidesToShow: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 2000
+    // });
     jQuery(".slider-playroom").slick({
         arrows: true,
         slidesToShow: 1,
@@ -30,3 +30,25 @@ jQuery(document).ready(function () {
         autoplaySpeed: 1500
     });
 })
+
+let preview_sl = new Swiper(".preview-slider", {
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    }
+});
+
+let sliderBlock = document.querySelector(".preview-slider");
+
+sliderBlock.addEventListener("mouseenter", function (e) {
+    preview_sl.autoplay.stop();
+});
+sliderBlock.addEventListener("mouseleave", function (e) {
+    preview_sl.autoplay.start();
+});
